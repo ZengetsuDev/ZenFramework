c91177e868cb5619f4db09023168cff4511dbb35
ZenegtsuDev <emnitpro@gmail.com>
2016-01-31 11:33:27 +0100

    Copyright was wrote.
    Copyright was wrote.

--- src/com/zenframework/math/geometry/GeometricalMethods.java
+++ src/com/zenframework/math/geometry/GeometricalMethods.java
@@ -1,5 +1,18 @@
 package com.zenframework.math.geometry;
 
+/*
+ * ZenFramework v0.05.
+ * 
+ * This Framework is developed by a Zengetsu_.
+ * 
+ * All class of is developed for this Framework.
+ * If you don't use this Framework, you cannot use the classes.
+ * 
+ * Date: 31/01/2016 11:30 (Paris)
+ * 
+ * ©2016 Civiz. All rights reserved.
+ */
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 
